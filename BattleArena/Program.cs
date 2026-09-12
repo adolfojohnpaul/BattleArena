220529148099b838e01ba5f580b18095792705a5
adolfojohnpaul <jestoniadolfo94@gmail.com>
2026-09-11 23:04:00 -0700

    Refactor Battle Logic Add Fighter, Marksman and Tank Classes

--- BattleArena/Program.cs
+++ BattleArena/Program.cs
@@ -1,36 +1,38 @@
-﻿using BattleArena.Warriors;
+﻿using battleArena.Warrior;
+using BattleArena.warriors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace BattleArena
+namespace battleArena
 {
-
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            Warrior Raymond = new Warrior("Raymond", 100, 30, "Sinuntok");
-            Warrior Kirk = new Warrior("Kirk", 200, 15, "Sinipa");
-            Warrior Athlon = new Warrior("Athlon", 150, 20, "Sinargo");
+            var Raymond = new Marksman("Raymond", 100, 30, 20);
+            var Kirk = new Tank("Kirk", 200, 25, 15);
+            var Athlon = new Fighter("Athlon", 100, 30, 20);
+
 
             Raymond.DisplayStatus();
             Kirk.DisplayStatus();
-            Athlon.DisplayStatus();
 
-            int round = 1;
-            while (Raymond.IsAlive && Kirk.IsAlive && Athlon.IsAlive)
+            while (Raymond.IsAlive && Kirk.IsAlive)
             {
-                Console.WriteLine($"----------Round {round}-----------------");
+                Console.WriteLine("\n\n======================================");
                 Raymond.Attack(Kirk);
-                Kirk.Attack(Raymond);
-                Athlon.Attack(Kirk);
-                Console.WriteLine("---------------------------");
-                round++;
+                Console.WriteLine("--------------------------------");
+                Thread.Sleep(20000);
+                Raymond.Attack(Kirk);
+                Thread.Sleep(20000);
+
             }
+
+            Console.ReadKey();
         }
     }
 }
--- BattleArena/Warriors/Warrior.cs
+++ BattleArena/Warriors/Warrior.cs
@@ -1,17 +1,22 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BattleArena.Warriors
+namespace battleArena.Warrior
 {
-    internal class Warrior
+    public abstract class Warrior
     {
         private bool _IsAlive;
+
         public string Name { get; private set; }
-        public string SpecialAttackName { get; private set; }
+
+        public string SpecialAttackNAme { get; set; }
+
         public int Health { get; private set; }
+
         public int AttackPower { get; private set; }
 
         public bool IsAlive
@@ -24,36 +29,30 @@ namespace BattleArena.Warriors
             private set { _IsAlive = value; }
         }
 
-        public Warrior(string name, int health, int attackPower, string specialAttackName)
+
+        public Warrior(string name, int health, int attackPower)
         {
             Name = name;
             Health = health;
             AttackPower = attackPower;
-            SpecialAttackName = specialAttackName;
         }
-        private void TakeDamage(int damage)
+
+        protected virtual void TakeDamage(int damage)
         {
             Health -= damage;
             if (Health < 0) Health = 0;
         }
 
-        public void Attack(Warrior target)
+
+
+        public virtual void DisplayStatus()
         {
-            TakeDamage(target.AttackPower);
-            Console.WriteLine($"{SpecialAttackName} ni {Name} si {target.Name}");
-            Console.WriteLine($"{target.Name} takes {AttackPower} damage! ");
-            Console.WriteLine($"{target.Name} has {target.Health} health remaining.");
-            Console.WriteLine("----------------------------------------------\n");
+            Console.WriteLine($"--== {Name} ==--");
+            Console.WriteLine($"\t[*] Health: {Health}");
+            Console.WriteLine($"\t[*] Attack Power: {AttackPower}");
+
         }
 
-        public void DisplayStatus()
-        {
-            Console.WriteLine($"\t---{Name}---");
-            Console.WriteLine($"\t[+] Health: {Health}");
-            Console.WriteLine($"\t[+] AttackPower: {AttackPower}");
-            Console.WriteLine($"\t[+] Special Attack: {SpecialAttackName}");
-            Console.WriteLine("----------------------------------------------");
-        }
-
+        public abstract void Attack(Warrior target);
     }
 }
