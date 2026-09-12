d03834dd40b9fccce46cb86354da2d8a4851e02e
adolfojohnpaul <jestoniadolfo94@gmail.com>
2026-09-11 22:41:02 -0700

    Intro to Constructor and Properties

--- BattleArena/Program.cs
+++ BattleArena/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BattleArena.Warriors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,27 @@ using System.Threading.Tasks;
 
 namespace BattleArena
 {
-    class Program
+
+
+    internal class Program
     {
         static void Main(string[] args)
         {
+            Warrior Raymond = new Warrior("Raymond", 100, 30);
+            Warrior Kirk = new Warrior("Kirk", 200, 15);
+            Warrior Athlon = new Warrior("Athlon", 150, 20);
+
+            Console.WriteLine($"{Raymond.Name} has " +
+                $"{Raymond.Health} health and {Raymond.AttackPower} " + $"attack power.");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"{Kirk.Name} has " +
+                $"{Kirk.Health} health and {Kirk.AttackPower} attack power.");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"{Athlon.Name} has " +
+                $"{Athlon.Health} health and {Athlon.AttackPower} attack power.");
+            Console.WriteLine("-----------------------------------");
+
+            Console.ReadKey();
         }
     }
 }
--- BattleArena/Warriors/Warrior.cs
+++ BattleArena/Warriors/Warrior.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace BattleArena.Warriors
 {
-    class Warrior
+    public class Warrior
     {
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+        public int AttackPower { get; private set; }
+
+
+        public Warrior(string name, int health, int attackPower)
+        {
+            Name = name;
+            Health = health;
+            AttackPower = attackPower;
+        }
     }
 }
